85867eaf65865c0ccb2cc6b118be5a961ec2272d
Julien Blanc <julien.blanc@tgcm.eu>
2025-11-08 11:03:17 +0000

    POC - Fix #58
    * this is a quick POC that fixes #58
    * not ready to merge as-is

--- include/boost/describe/enum.hpp
+++ include/boost/describe/enum.hpp
@@ -17,6 +17,7 @@
 #include <boost/describe/detail/pp_for_each.hpp>
 #include <boost/describe/detail/list.hpp>
 #include <type_traits>
+#include <algorithm>
 
 namespace boost
 {
@@ -30,6 +31,25 @@ template<class D> struct enum_descriptor
     // can't use auto here because of the need to supply the definitions below
     static constexpr decltype(D::value()) value = D::value();
     static constexpr decltype(D::name()) name = D::name();
+};
+
+template <std::size_t len>
+struct compile_time_string
+{
+	char data_[len] = {};
+	constexpr explicit compile_time_string(char const (&str)[len]){
+		std::copy_n(str, len, data_);
+	}
+	constexpr char const* data() const { return data_; }
+};
+template <std::size_t N>
+compile_time_string(char const (&str)[N]) -> compile_time_string<N>;
+
+
+template <class D, D v, int N, compile_time_string<N> S>
+struct _boost_desc {
+	static constexpr auto value() noexcept { return v; }
+	static constexpr auto name() noexcept { return S.data(); }
 };
 
 #ifndef __cpp_inline_variables
@@ -47,11 +67,14 @@ template<class... T> auto enum_descriptor_fn_impl( int, T... )
     inline auto boost_enum_descriptor_fn( E** ) \
     { return boost::describe::detail::enum_descriptor_fn_impl( 0
 
-#define BOOST_DESCRIBE_ENUM_ENTRY(E, e) , []{ struct _boost_desc { \
-    static constexpr auto value() noexcept { return E::e; } \
-    static constexpr auto name() noexcept { return #e; } }; return _boost_desc(); }()
+#define BOOST_DESCRIBE_ENUM_ENTRY_B(E, e) boost::describe::detail::_boost_desc<E, E::e, sizeof(#e), boost::describe::detail::compile_time_string<sizeof(#e)>(#e)>{}
+#define BOOST_DESCRIBE_ENUM_ENTRY(E, e) , BOOST_DESCRIBE_ENUM_ENTRY_B(E, e)
 
 #define BOOST_DESCRIBE_ENUM_END(E) ); }
+
+#define BOOST_DESCRIBE_ENUM_ENTRY_RETURN(E, e) , decltype(BOOST_DESCRIBE_ENUM_ENTRY_B(E, e)) 
+#define BOOST_DESCRIBE_NESTED_ENUM_RETURN_TYPE(E, First, ...) decltype(BOOST_DESCRIBE_ENUM_ENTRY_B(E, First)) \
+    	BOOST_DESCRIBE_PP_FOR_EACH(BOOST_DESCRIBE_ENUM_ENTRY_RETURN, E, ##__VA_ARGS__) \
 
 } // namespace detail
 
@@ -81,7 +104,12 @@ template<class... T> auto enum_descriptor_fn_impl( int, T... )
 
 #define BOOST_DESCRIBE_NESTED_ENUM(E, ...) \
     static_assert(std::is_enum<E>::value, "BOOST_DESCRIBE_NESTED_ENUM should only be used with enums"); \
-    BOOST_DESCRIBE_MAYBE_UNUSED friend BOOST_DESCRIBE_ENUM_BEGIN(E) \
+    BOOST_DESCRIBE_MAYBE_UNUSED friend \
+    auto boost_enum_descriptor_fn( E** ) \
+	    -> decltype(boost::describe::detail::enum_descriptor_fn_impl(0 \
+    BOOST_DESCRIBE_PP_FOR_EACH(BOOST_DESCRIBE_ENUM_ENTRY, E, ##__VA_ARGS__) \
+	 )) \
+    { return boost::describe::detail::enum_descriptor_fn_impl( 0 \
     BOOST_DESCRIBE_PP_FOR_EACH(BOOST_DESCRIBE_ENUM_ENTRY, E, ##__VA_ARGS__) \
     BOOST_DESCRIBE_ENUM_END(E)
 
